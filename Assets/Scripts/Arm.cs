361035ff2cfb892aebbff2ce5dc9c4705e695bb7
Arye <dabergshoeff@gmail.com>
2021-01-30 00:26:20 +0100

    Added DOTween and randomized enemy & objects

---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Arm : MonoBehaviour
+{
+    private Animator myAnimator;
+    private Crow playerInTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTrigger == null)
+            return;
+
+        if(playerInTrigger.swoopTimer > 0f || playerInTrigger.MyLight.enabled) {
+            myAnimator.SetTrigger("Rise");
+        }
+    }
+
+    private void CheckForHit() {
+        if(playerInTrigger != null) {
+            playerInTrigger.GetComponent<Rigidbody>().useGravity = true;
+            playerInTrigger.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            playerInTrigger.enabled = false;
+            Invoke("RestartGame", 3f);
+        }
+    }
+
+    private void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player"))
+            return;
+
+        playerInTrigger = other.gameObject.GetComponent<Crow>();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (!other.CompareTag("Player"))
+            return;
+
+        playerInTrigger = null;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (!collision.collider.CompareTag("Player"))
+            return;
+
+        CheckForHit();
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> ObjectPrefabs;
+    public List<GameObject> EnemyPrefabs;
+
+    public int AmountOfObjects;
+    public int AmountOfEnemies;
+
+    private List<GameObject> ObjectPositions;
+    private List<GameObject> EnemyPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectPositions = new List<GameObject>(GameObject.FindGameObjectsWithTag("ObjectSpawn"));
+        EnemyPositions = new List<GameObject>(GameObject.FindGameObjectsWithTag("EnemySpawn"));
+
+        SpawnFromPositions(ObjectPrefabs, ObjectPositions, AmountOfObjects);
+        SpawnFromPositions(EnemyPrefabs, EnemyPositions, AmountOfEnemies);
+    }
+
+    private void SpawnFromPositions(List<GameObject> prefabs, List<GameObject> positions, int amount) {
+        for (int i = 0; i < amount; i++) {
+            GameObject pos = positions[Random.Range(0, positions.Count)];
+            GameObject go = Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+            go.transform.position = pos.transform.position;
+            positions.Remove(pos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
