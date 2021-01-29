43ac8a9c21e6d8593a1f06ca64a1752551d64bb7
Arye <dabergshoeff@gmail.com>
2021-01-29 21:38:03 +0100

    Added continuous movement & pickup

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Player;
+
+    private Vector3 relativePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        relativePos = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + relativePos;
+    }
+}
