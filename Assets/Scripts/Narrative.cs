cbd71e85c007600d1a57b099cc3877704e26de2e
Arye <dabergshoeff@gmail.com>
2021-01-30 22:06:37 +0100

    Fixed tag

--- Assets/Scripts/Narrative.cs
+++ Assets/Scripts/Narrative.cs
@@ -79,6 +79,7 @@ public class Narrative : MonoBehaviour
             PlayPoem(Emptyhanded);
         }
         else {
+            c.ObjectHeld.tag = "Untagged";
             c.DropObject();
 
             itemsBrought++;
